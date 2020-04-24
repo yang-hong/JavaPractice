b93a72fd56bb08eb6f0acb73374422cb48367df9
Yang Hong <yang-hong1994@outlook.com>
2020-04-24 16:19:06 -0500

    Add files via upload

---
+++ Web Application with REST API + AJAX/WebContent/js/git.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$.ajax( {
+		url:"https://api.github.com/users"
+	}).then(function(data){
+		console.log(data);
+		var table = $("#usersTable");
+		var contents = "";
+		$.each(data, function(idx){
+			contents += "<tr>";
+			contents += "<td>" + data[idx].id + "</td>";
+			contents += "<td>" + data[idx].login + "</td>";
+			contents += "<td>" + data[idx].followers_url + "</td>";
+			contents += "<td>" + data[idx].login + "</td>";
+			contents += "<td><img src='" + data[idx].avatar_url + "' width=50 height=50 /></td>";
+			contents += "</tr>";
+		})
+		table.append(contents);
+	});
+});
+
+
